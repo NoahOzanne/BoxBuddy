b06e739f2b710105b444b4d1f6d0551bc42182f9
Noah <your.email@example.com>
2025-03-29 11:02:45 +1100

    ghd

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-  //test test
+  //test test hg
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
